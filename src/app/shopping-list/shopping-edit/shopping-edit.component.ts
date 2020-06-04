22a4aa3264b206f2fef6edb5a7edd550136f23e6
Rakesh Jain <rakeshvora2007@gmail.com>
2020-06-03 23:47:16 -0400

    forms #2

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
-import { FormControl, NgForm } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { ShoppingListService } from '../shopping-list.service';
 import { Ingredient } from 'src/app/shared/ingredient.model';
 
@@ -9,18 +9,17 @@ import { Ingredient } from 'src/app/shared/ingredient.model';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit {
-  @ViewChild('nameInput') nameInputRef: ElementRef;
-  @ViewChild('amountInput') amountInputRef: ElementRef;
 
   constructor(private slService: ShoppingListService) {}
 
   ngOnInit(): void {}
 
-  onAdd() {
+  onAddItem(form: NgForm) {
+    const value = form.value;
     this.slService.addIngredient(
       new Ingredient(
-        this.nameInputRef.nativeElement.value,
-        this.amountInputRef.nativeElement.value
+        value.name,
+        value.amount
       )
     );
   }
