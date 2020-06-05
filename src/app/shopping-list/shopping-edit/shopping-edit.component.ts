83e75a3232f3118a42ebb24d17263e51d2df4e59
Rakesh Jain <rakeshvora2007@gmail.com>
2020-06-05 13:00:00 +0530

    shopping-edit update

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,30 +1,55 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, OnDestroy } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { ShoppingListService } from '../shopping-list.service';
 import { Ingredient } from 'src/app/shared/ingredient.model';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-shopping-edit',
   templateUrl: './shopping-edit.component.html',
   styleUrls: ['./shopping-edit.component.css']
 })
-export class ShoppingEditComponent implements OnInit {
+export class ShoppingEditComponent implements OnInit, OnDestroy {
+  subscription: Subscription;
+  @ViewChild('fo') slForm: NgForm;
+  editMode = false;
+  selectedItemIndex: number;
 
-  constructor(private slService: ShoppingListService) {}
+  constructor(private slService: ShoppingListService) {
+    this.subscription = this.slService.selectedIngredientItem.subscribe((selectedItem) => {
+      this.selectedItemIndex = selectedItem.index;
+      this.editMode = true;
+      this.slForm.setValue({
+        name: selectedItem.ingredient.name,
+        amount: selectedItem.ingredient.amount
+      });
+    });
+  }
 
   ngOnInit(): void {}
 
-  onAddItem(form: NgForm) {
-    const value = form.value;
-    this.slService.addIngredient(
-      new Ingredient(
-        value.name,
-        value.amount
-      )
-    );
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
+
+  clearForm() {
+    this.slForm.reset();
+    this.editMode = false;
+  }
+
+  onDelete() {
+    this.slService.deleteIngredient(this.selectedItemIndex);
+    this.clearForm();
   }
 
   onSubmit(form: NgForm) {
-    console.log(form);
+    const value = form.value;
+    const newIngredient = new Ingredient(value.name, value.amount);
+    if (this.editMode) {
+      this.slService.updateIngredient(this.selectedItemIndex, newIngredient)
+    } else {
+      this.slService.addIngredient(newIngredient);
+    }
+    this.clearForm();
   }
 }
