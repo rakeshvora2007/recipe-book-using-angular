59e9c99095d719200bf81331af58561cc070db81
Rakesh Jain <rakeshvora2007@gmail.com>
2020-06-10 23:55:36 -0400

    react form #3

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -20,8 +20,8 @@ export class RecipeEditComponent implements OnInit {
 
   ngOnInit(): void {
     this.route.params.subscribe((params: Params) => {
-      this.id = +params['id'];
-      this.editMode = params['id'] !== undefined;
+      this.id = +params.id;
+      this.editMode = params.id !== undefined;
       this.onInitForm();
     });
   }
